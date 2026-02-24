1cda5635782c71de38cc281b7bb0c0bd899fb249
JustinVillegas22 <jpv9q@virginia.edu>
2026-02-24 00:20:58 +0000

    Fix /moon prerender on Vercel

--- src/app/moon/page.tsx
+++ src/app/moon/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useMemo, useState } from "react";
